de24e146e0160ff14d3ac9ed7bfcc38b573b0ddf
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-09-22 19:45:43 +0000

    correction of typed text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
-num_letters = int(input('How many letters should it has: '))
-num_symbols = int(input('How many symbols should it has: '))
-num_numbers = int(input('How many numbers should it has: '))
+num_letters = int(input('How many letters should it have: '))
+num_symbols = int(input('How many symbols should it have: '))
+num_numbers = int(input('How many numbers should it have: '))
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 symbols = ['$', '%', '@', '#', '&', '*']
